83b5a0d10964c0769a759af30a7d306566c54c08
fafcrumb <bri.ferch@gmail.com>
2016-11-06 02:15:35 -0500

    uh oh one file

--- square.js
+++ square.js
@@ -1,5 +1,6 @@
 var canvas;
 var ctx;
+var socket;
 
 var viewportWidth = $(document).width();
 var viewportHeight = $(document).height();
@@ -7,10 +8,21 @@ var viewportHeight = $(document).height();
 var myPlayer;
 var otherPlayer;
 
+var players = [];
+
+var submitButton = $("#submit");
+var status = $("#status")
+var nameListArea = $("#name_list_area");
+var nameStartUp = $("#name_start_up");
+var nameList = $("#nameList");
+nameListArea.hide();
+
 class Player {
   constructor(name, color) {
     this.name = name;
     this.color = color;
+
+    this.uuid = '';
 
     this.xPos = 0;
     this.yPos = 0;
@@ -92,6 +104,7 @@ function highlightLinks() {
 }
 
 (function init() {
+  socket = io.connect("ws://128.205.27.232:4004/");
   canvas = document.createElement('canvas');
   canvas.id = "gameCanvas";
   ctx = canvas.getContext("2d");
@@ -117,26 +130,34 @@ function run() {
 var keys = [];
 
 function input() {
-  if (keys[37]) {
+  if (keys[37]) { //left
     if (myPlayer.xVel > -myPlayer.speed) {
       myPlayer.xVel--;
     }
   }
-  if (keys[39]) {
+  if (keys[39]) { //right
     if (myPlayer.xVel < myPlayer.speed) {
       myPlayer.xVel++;
     }
   }
-  if (keys[38]) {
+  if (keys[38]) { //up
     if (myPlayer.yVel > -myPlayer.speed) {
       myPlayer.yVel--;
     }
   }
-  if (keys[40]) {
+  if (keys[40]) { //down
     if (myPlayer.yVel < myPlayer.speed) {
       myPlayer.yVel++;
     }
   }
+
+  socket.emit("playerInfo", JSON.stringify({
+    "action" : "direction",
+    "up_press" : keys[38],
+    "down_press" : keys[40],
+    "left_press" : keys[37],
+    "right_press" : keys[39]
+  }));
 }
 
 function update() {
@@ -146,7 +167,7 @@ function update() {
 function render() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  myPlayer.render();
+  myPlayer.render(ctx);
 
   portals.forEach(function(portal) {
     portal.render(ctx);
@@ -162,6 +183,52 @@ document.body.addEventListener("keydown", function(e) {
 document.body.addEventListener("keyup", function(e) {
   e.preventDefault();
   keys[e.keyCode] = false;
+});
+
+socket.on('onconnected', function( data ) {
+    console.log( 'Connected successfully to the socket.io server. My server side ID is ' + data.id );
+});
+
+function requestPlay() {
+
+  // players[0].uuid
+}
+
+socket.on('playerInfo', function(data) {
+  console.log( 'data recieved: ' + data );
+  var rJson = JSON.parse(data);
+  console.log(data);
+  switch(rJson.action) {
+    case "create_player":
+      myPlayer.uuid = rJson.uuid;
+      break;
+    case "in_lobby":
+      if(rJson.players != null) {
+        nameListArea.show();
+        players = [];
+        JSON.parse(rJson.players).forEach(function(item,index) {
+          var temp = $("<li class='list-group-item'>").html(item[Object.keys(item)[0]]);
+          nameList.append(temp);
+          players.push(item);
+        });
+      }
+      break;
+  }
+});
+
+submitButton.click(function() {
+  player.name=$("#name_input").val();
+  socket.emit("playerInfo", JSON.stringify({
+    "action" : "create_player",
+    "name" : player.name,
+    "uuid" : player.uuid
+  }));
+  $("#name_area").text(player.name);
+  console.log(player.name);
+  nameStartUp.hide();
+  socket.emit("playerInfo",JSON.stringify({
+    "action" : "in_lobby"
+    }));
 });
 
 jQuery(window).resize(function(){
